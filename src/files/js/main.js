bc061370dcebc39165f87a3700a7869abdbbb1eb
jheytompkins <jh3y@users.noreply.github.com>
2014-03-15 21:43:24 +0000

    fixed demo for page and set up a DocPad project to maintain it. Was broke for some reason.

---
+++ src/files/js/main.js
@@ -0,0 +1,22 @@
+//Feed me javascript here.
+var setDemo = function (name, element) {
+	$('.demo-container').remove();
+	$('.show').removeClass('show');
+	$('div[name="' + name + '"').addClass('show');
+	$('body').prepend(element);
+};
+$('.action').on('click', function (event) {
+	var action = $(this).attr('name');
+	switch(action) {
+	case "basic":
+		setDemo('basic', '<div class = "demo-container progrecss fixed blue" data-progrecss = "0"></div>')
+		setTimeout(function () {$('.demo-container').attr('data-progrecss', '75');}, 1000);
+		break;
+	case "mock":
+		setDemo('mock', '<div class = "demo-container progrecss mock fixed red" data-progrecss-mock = "5"></div>');
+		break;
+	case "staggered-mock":
+		setDemo('staggered-mock', '<div class = "demo-container progrecss fixed mock staggered green" data-progrecss-mock = "5"></div>');
+		break;
+	}
+});
