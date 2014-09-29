35cfcad59e460b7bc4e158df3bb39268447e9c4f
jh3y <jhey.tompkins@gmail.com>
2014-09-29 23:46:40 +0100

    Update page for progression

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var gulp = require('gulp'),
       styles: [
         'src/vendor/normalize.css/normalize.css',
         'src/vendor/bootstrap/dist/css/bootstrap.css',
-        'src/vendor/csspinner/build/csspinner.min.css'
+        'src/vendor/progression/dist/progression.min.css'
       ]
     }
   },
